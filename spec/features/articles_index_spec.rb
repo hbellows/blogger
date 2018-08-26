823a23cffe6d668e063dc752e28262ae82d11fe2
Harper Bellows <harper.bellows@gmail.com>
2018-08-26 09:37:46 -0600

    add create new article spec

--- spec/features/articles_index_spec.rb
+++ spec/features/articles_index_spec.rb
@@ -6,7 +6,7 @@ describe 'Articles Index' do
       article_1 = Article.create(title: 'Title 1', body: 'Body 1')
       article_2 = Article.create(title: 'Title 2', body: 'Body 2')
 
-      visit '/articles'
+      visit articles_path
 
       expect(page).to have_content(article_1.title)
       expect(page).to have_content(article_1.body)
@@ -15,6 +15,24 @@ describe 'Articles Index' do
 
       expect(page).to have_link(article_1.title)
       expect(page).to have_link("Create a New Article")
+    end
+  end
+  describe 'user creates a new article' do
+    describe 'user links from index to create new article form' do
+      it 'creates a new article' do
+
+        visit articles_path
+        click_link("Create a New Article")
+
+        expect(current_path).to eq(new_article_path)
+
+        fill_in("article[title]", with: "New Title!")
+        fill_in("article[body]", with: "New Body!")
+        click_on("Create Article")
+
+        expect(page).to have_content("New Title!")
+        expect(page).to have_content("New TBody!")
+      end
     end
   end
 end
