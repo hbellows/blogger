6034b8755e1fa930be253e8469f3ef1906d77f15
Harper Bellows <harper.bellows@gmail.com>
2018-08-26 14:01:22 -0600

    add flash message

--- spec/features/article_edit_spec.rb
+++ spec/features/article_edit_spec.rb
@@ -10,6 +10,17 @@ describe 'Article Edit' do
       click_link("Edit")
 
       expect(current_path).to eq(edit_article_path(article))
+      fill_in "article[title]", with: "Different Title"
+      fill_in "article[body]", with: "Different Body"
+      click_on "Update Article"
+
+      expect(current_path).to eq(article_path(article))
+
+      expect(page).to have_content("Different Title")
+      expect(page).to_not have_content("Title 1")
+      expect(page).to have_content("Different Body")
+      expect(page).to_not have_content("Body 1")
+      expect(page).to have_content("Article Different Title Updated!")
     end
   end
 end
